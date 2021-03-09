dd804677ef9a1b58b94172bec19bceed64d6b1b6
PzSnark <pzsnark@gmail.com>
2021-03-09 18:11:38 +0400

    current

---
+++ conf.py
@@ -0,0 +1,4 @@
+AD_SERVER = 'dc2.gk.local'
+AD_USER = 'ldap-bot@gk.local'
+AD_PASSWORD = '12345678'
+AD_SEARCH_TREE = 'dc=gk,dc=local'
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import ldap3
+from ldap3 import Connection, SUBTREE
+from conf import *
 
+server = ldap3.Server(AD_SERVER)
+conn = Connection(server, user=AD_USER, password=AD_PASSWORD)  # вынести реквизиты в окружение ОС
+conn.bind()
+
+conn.search(AD_SEARCH_TREE, '(&(objectCategory=Person)(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(&(company=*)))',
+            SUBTREE,
+            attributes=['cn', 'department', 'sAMAccountName', 'displayName', 'telephoneNumber',
+                        'ipPhone', 'title', 'manager', 'company', 'lastLogon']
+            )
+
+print(conn.entries)
+
+# for entry in conn.entries:
+#     print(entry.cn, entry.company)
+
+# '(&(objectCategory=Person)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))'
