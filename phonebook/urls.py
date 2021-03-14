90ec094b98e82c1600857ec6160b3553fd2eb5c7
PzSnark <pzsnark@gmail.com>
2021-03-14 19:34:48 +0400

    current

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('abz', views.abz, name='abz'),
+    path('query', views.query, name='query'),
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def server_request():
     server = ldap3.Server(AD_SERVER)
     conn = Connection(server, user=AD_USER, password=AD_PASSWORD)  # вынести реквизиты в окружение ОС
     conn.bind()
@@ -16,8 +16,34 @@ def index(request):
                 attributes=['department', 'sAMAccountName', 'displayName', 'physicalDeliveryOfficeName',
                             'telephoneNumber', 'mail', 'mobile', 'title', 'company', 'lastLogon']
                 )
+    return conn.entries
 
+
+def index(request):
     context = {
-        'conn': conn.entries
+        'entries': server_request()
     }
     return render(request, 'phonebook/index.html', context)
+
+
+def abz(request):
+    entries = []
+    for entry in server_request():
+        if entry.company == 'ABZ':
+            entries.append(entry)
+    context = {
+        'entries': entries
+    }
+    return render(request, 'phonebook/index.html', context)
+
+
+def query(request):
+    entries = {}
+    for entry in server_request():
+        entries.update({entry.company: list(entry.company).append(entry.company)})
+    context = {
+        'entries': entries
+    }
+    return render(request, 'phonebook/index.html', context)
+
+# найти все уникальные вхождения в entrie.company из них формировать список фильтра
