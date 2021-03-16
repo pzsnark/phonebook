6a27d224401d9969e8dc5101c2f2fc5bda5c5c1b
PzSnark <pzsnark@gmail.com>
2021-03-16 20:03:18 +0400

    current

--- phonebook_django/settings.py
+++ phonebook_django/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'phonebook.apps.PhonebookConfig',
-    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
