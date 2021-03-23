3368c6f38636d25d867ac36ba694727c57ffe6b0
PzSnark <pzsnark@gmail.com>
2021-03-23 21:54:08 +0400

    current

---
+++ phonebook/ldap.py
@@ -0,0 +1,31 @@
+import ldap3
+from ldap3 import Connection, SUBTREE
+from django.shortcuts import render
+
+AD_SERVER = 'dc2.gk.local'
+AD_USER = 'ldap-bot@gk.local'
+AD_PASSWORD = '12345678'
+AD_SEARCH_TREE = 'dc=gk,dc=local'
+
+
+def server_request():
+    server = ldap3.Server(AD_SERVER)
+    conn = Connection(server, user=AD_USER, password=AD_PASSWORD)  # вынести реквизиты в окружение ОС
+    conn.bind()
+
+    conn.search(AD_SEARCH_TREE,
+                '(&(objectCategory=Person)(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(&(company=*)))',
+                SUBTREE,
+                attributes=['department', 'sAMAccountName', 'displayName', 'physicalDeliveryOfficeName',
+                            'telephoneNumber', 'mail', 'mobile', 'title', 'company', 'lastLogon']
+                )
+    return conn.entries
+
+
+entries = server_request()
+print(sorted(entries, key=lambda x: x.company))
+print(type(entries))
+for entry in entries:
+    print(entry)
+
+# поискать __lt__ в классе
