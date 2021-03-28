92c8258f569f3a865d012c9b1e653e21e34a72a9
PzSnark <pzsnark@gmail.com>
2021-03-28 13:46:40 +0400

    current

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -6,5 +6,6 @@ app_name = "phonebook"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:company>', views.quest, name='company'),
+    path('<str:company>', views.filter_by_company, name='company'),
+    path('test/<str:company>', views.test2, name='test'),
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -1,7 +1,11 @@
 import ldap3
-from ldap3 import Connection, SUBTREE
-from .conf import *
 from django.shortcuts import render
+from ldap3 import Connection, SUBTREE
+
+AD_SERVER = 'dc2.gk.local'
+AD_USER = 'ldap-bot@gk.local'
+AD_PASSWORD = '12345678'
+AD_SEARCH_TREE = 'dc=gk,dc=local'
 
 
 def server_request():
@@ -18,6 +22,130 @@ def server_request():
     return conn.entries
 
 
+class Employee:
+    """Запись из Active Directory (Person)"""
+
+    def __init__(self, account_name, display_name, company, title, department, office, phone, mobile, email, last_logon):
+        self._account_name = account_name
+        self._display_name = display_name
+        self._company = company
+        self._title = title
+        self._department = department
+        self._office = office
+        self._phone = phone
+        self._mobile = mobile
+        self._email = email
+        self._last_logon = last_logon
+
+    def __str__(self):
+        return self._account_name
+
+    # def __eq__(self, other):
+    #     return self.account_name == other.account_name
+
+    def __lt__(self, other):
+        return self.account_name < other.account_name
+
+    # def __gt__(self, other):
+    #     return self.account_name > other.account_name
+
+    @property
+    def account_name(self):
+        return self._account_name
+
+    @property
+    def display_name(self):
+        return self._display_name
+
+    @property
+    def company(self):
+        return self._company
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def department(self):
+        return self._department
+
+    @property
+    def office(self):
+        return self._office
+
+    @property
+    def phone(self):
+        return self._phone
+
+    @property
+    def mobile(self):
+        return self._mobile
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def last_logon(self):
+        return self._last_logon
+
+
+class EmployeeList:
+    """Список объектов класса Employee"""
+
+    def __init__(self):
+        self.employee_list = []
+
+    @property
+    def register(self):
+        return self.employee_list
+
+    @register.setter
+    def register(self, value):
+        self.employee_list.append(value)
+
+    def company(self, company):
+        filter_company = []
+        if company == 'All':
+            return self.employee_list
+        else:
+            for entry in self.employee_list:
+                if entry.company == company:
+                    filter_company.append(entry)
+            return filter_company
+
+    def is_sorting(self):
+        employee_list_sort = sorted(self.employee_list)
+        return employee_list_sort
+
+
+def transfer(entries):
+    employers = EmployeeList()
+    for entry in entries:
+        employers.register = (Employee(
+            account_name=entry.sAMAccountName,
+            display_name=entry.displayName,
+            company=entry.company,
+            title=entry.title,
+            department=entry.department,
+            office=entry.physicalDeliveryOfficeName,
+            phone=entry.telephoneNumber,
+            mobile=entry.mobile,
+            email=entry.mail,
+            last_logon=entry.lastLogon
+        ))
+    return employers
+
+
+def test2(request, company='All'):
+    employers = transfer(server_request())
+    employers.is_sorting()
+    context = {
+        'entries': employers.company(company=company),
+    }
+    return render(request, 'phonebook/test.html', context)
+
+
 def index(request):
     entries = server_request()
     entries.sort()
@@ -28,7 +156,7 @@ def index(request):
     return render(request, 'phonebook/index.html', context)
 
 
-def quest(request, company):
+def filter_by_company(request, company):
     entries = []
     for entry in server_request():
         if entry.company == company:
